caf38aab0163f46a81ab694cba62fbc72151d036
Emmett Jaakkola <56277507+Emmennater@users.noreply.github.com>
2025-10-14 22:11:22 -0500

    Fixed train

--- train.py
+++ train.py
@@ -3,7 +3,7 @@ from data_loader import *
 from pathlib import Path
 
 cwd = Path(__file__).parent
-data_dir = cwd / 'POP902_cleaned'
+data_dir = cwd / 'POP9092_cleaned'
 
 def save_model(model: MusicGen, epochs, path='music_gen.pt'):
   """Save the MusicGen model
